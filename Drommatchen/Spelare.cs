19db3966f4dde60b15ec63885d285ebc8e26f181
david <davidbillfeldt@gmail.com>
2026-09-17 14:32:22 +0200

    Fixed code hade new players in the konstruktor

--- Drommatchen/Spelare.cs
+++ Drommatchen/Spelare.cs
@@ -1,35 +1,24 @@
 public class Spelare
 {
-    // Privata fält
-    public string _namn { get; private set; }
-    public int _nummer { get; private set; }
-    public string _position { get; private set; }
-
-    // Properties — publik get, privat set
-
     public string Namn { get; private set; }
-
-
     public int Nummer { get; private set; }
-    // Skapa get och privat set för Nummer
-
     public string Position { get; private set; }
-    // Skapa get och privat set för Position
-
-
-    // Konstruktor
     public Spelare(string namn, int nummer, string position)
     {
-        _namn = namn;
-        _nummer = nummer;
-        _position = position;
-
+        Namn = namn;
+        Nummer = nummer;
+        Position = position;
     }
-
-    //Tog hjälp av Ai på rad 21 då jag hade skrivt ett ; efter raden som strulade till koden:)
-   
-    Spelare spelare1 = new Spelare("Faker", 7, "Midlane");
-    Spelare spelare2 = new Spelare("Caps", 10, "Midlane");
-
-    // TODO: tilldela de privata fälten
+}
+public class Program
+{
+    public static void Main()
+    {
+        Match match1 = new Match("T1", "G2", "2026-09-17");
+        Spelare spelare1 = new Spelare("Faker", 7, "Midlane");
+        Spelare spelare2 = new Spelare("Caps", 10, "Midlane");
+        match1.Presentera();
+        Console.WriteLine();
+        match1.AnnounceraKillLeader(spelare1);
+    }
 }
