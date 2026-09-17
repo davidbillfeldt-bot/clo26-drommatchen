59e1a2cd2227513d5c0273fb2f78b6055574f076
david <davidbillfeldt@gmail.com>
2026-09-17 14:17:07 +0200

    Changed back an error in private string_ hemmalag;

--- Drommatchen/Match.cs
+++ Drommatchen/Match.cs
@@ -1,36 +1,32 @@
 public class Match
 {
-    // Privata fält
-    public string _hemmalag { get; private set; }
-    public string _bortalag { get; private set; }
-    public string _datum { get; private set; }
-
-    // Properties — publik get, privat set
-    public string Hemmalag { get; private set; }
-
-    public string Bortalag {  get; private set; }
-    public string Datum {  get; private set; }
+    private string _hemmalag;
+    private string _bortalag;
+    private string _datum;
 
     // Pluspoäng om ni kommer på hur man kan minska ner koden med properties :)
 
     // Konstruktor
     public Match(string hemmalag, string bortalag, string datum)
     {
-       _bortalag = hemmalag;
-       _bortalag = bortalag;
-       _datum = datum;
-        Match match1 = new Match("T1", "G2", "2026-09-17");
+        _hemmalag = hemmalag;
+        _bortalag = bortalag;
+        _datum = datum;
     }
 
     // Metod 1: skriv ut matchens lag och datum
     public void Presentera()
     {
-        // TODO
+        Console.WriteLine(_hemmalag + " mot " + _bortalag + " - " +_datum);
     }
 
     // Metod 2: skriv ut spelarens namn, nummer och position
-    public void AnnounceraMålskytt(Spelare spelare)
+    public void AnnounceraKillLeader(Spelare spelare)
+   
     {
-        // TODO
+        Console.WriteLine(spelare.Namn);
+        Console.WriteLine(spelare.Nummer);
+        Console.WriteLine(spelare.Position);
     }
+
 }
